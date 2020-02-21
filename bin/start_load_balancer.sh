7d16cd86e55c0b4df9bc47e5a20e9fc550f9e16a
mmmries <riesmmm@gmail.com>
2020-02-21 09:24:43 -0700

    allow websocket connections to the blog

--- bin/start_load_balancer.sh
+++ bin/start_load_balancer.sh
@@ -1,1 +1,1 @@
-docker run -d --volume="/root/load_balancer_nginx.conf:/etc/nginx/nginx.conf" --volume="/root/secrets:/etc/ssl/nginx" --network=host --name load-balancer nginx:1.15-alpine
+docker run -d --volume="/root/load_balancer_nginx.conf:/etc/nginx/nginx.conf" --volume="/root/secrets:/etc/ssl/nginx" --network=host --name load-balancer nginx:1.17-alpine
